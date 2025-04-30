2fe077cc81a4c3cc5704183e8a234977d6527964
linaaqu <latihewa2006@gmail.com>
2025-04-30 10:21:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,85 @@
 import sqlite3
+from tkinter import *
+import tkinter.ttk as ttk
 
-# Устанавливаем соединение с базой данных
-connection = sqlite3.connect('tasks.db')
-cursor = connection.cursor()
+conn = sqlite3.connect('example.db')
+cursor = conn.cursor()
 
-# Создаем таблицу Tasks
-cursor.execute('''CREATE TABLE IF NOT EXISTS Tasks (
-    id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    status TEXT DEFAULT 'Not Started',
-    priority INTEGER DEFAULT 2 CHECK(priority BETWEEN 0 AND 3)
-)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS notes (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   note TEXT, priority INTEGER)''')
 
-# Функция добавления новой задачи
-def add_task(title, priority=2):
-    cursor.execute('INSERT INTO Tasks (title, priority) VALUES (?, ?)', (title, priority))
-    connection.commit()
+def show_notes():
+    cursor.execute("SELECT * FROM notes")
+    rows = cursor.fetchall()
 
-# Функция обновления статуса задачи
-def update_task_status(task_id, status):
-    cursor.execute('UPDATE Tasks SET status = ? WHERE id = ?', (status, task_id))
-    connection.commit()
+    for i in tree.get_children():
+        tree.delete(i)
 
-# Функция вывода списка задач
-def list_tasks():
-    cursor.execute('SELECT * FROM Tasks ORDER BY priority DESC')
-    tasks = cursor.fetchall()
-    for task in tasks:
-        print(task)
+    for row in rows:
+        tree.insert("", "end", values=row)
 
-# Добавляем задачи с приоритетами
-add_task('Подготовить презентацию', 1)
-add_task('Закончить отчет', 3)
-add_task('Подготовить ужин')
 
-update_task_status(2, 'In Progress')
-list_tasks()
-connection.close()
+def add_note():
+    def add():
+        cursor.execute("INSERT INTO notes (note, priority) VALUES (?, ?)", (note.get(), priority.get()))
+        conn.commit()
+        show_notes() 
+        add_window.destroy()
+    add_window = Toplevel(window)
+    note = Entry(add_window)
+    note.grid()
+    priority = Entry(add_window)
+    priority.grid()
+    Button(add_window, text="Добавить", command=add).grid()
+
+def update_note():
+    def update():
+        cursor.execute("UPDATE notes SET note=?, priority=? WHERE id=?", (note.get(), priority.get(), id_.get()))
+        conn.commit()
+        show_notes()
+        update_window.destroy()
+    update_window = Toplevel(window)
+    id_ = Entry(update_window)
+    id_.grid()
+    note = Entry(update_window)
+    note.grid()
+    priority = Entry(update_window)
+    priority.grid()
+    Button(update_window, text="Обновить", command=update).grid()
+
+def delete_note():
+    def delete():
+        cursor.execute("DELETE FROM notes WHERE id=?", (id_.get(),))
+        conn.commit()
+        show_notes()
+        delete_window.destroy()
+    delete_window = Toplevel(window)
+    id_ = Entry(delete_window)
+    id_.grid()
+    Button(delete_window, text="Удалить", command=delete).grid()
+
+
+window = Tk()
+window.title("Заметки")
+window.geometry('600x400')
+
+
+
+tree = ttk.Treeview(window, columns=("ID", "Заметка", "Приоритет"), show="headings")
+tree.heading("ID", text="ID")
+tree.heading("Заметка", text="Заметка")
+tree.heading("Приоритет", text="Приоритет")
+tree.grid(row=5, column=0, columnspan=4, pady=(10, 0))
+
+
+Button(window, text="Добавить", command=add_note).grid(row=0, column=0, pady=5)
+Button(window, text="Обновить", command=update_note).grid(row=1, column=0, pady=5)
+Button(window, text="Удалить", command=delete_note).grid(row=2, column=0, pady=5)
+Button(window, text="Показать", command=show_notes).grid(row=3, column=0, pady=5)
+
+
+
+
+window.mainloop()
+conn.close()
